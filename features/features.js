7f602db7f4269fb56812fb525d2c918591259fbe
laughinapple <laughinapple1221@gmail.com>
2021-08-09 02:37:33 +0900

    interface validation

--- features/features.js
+++ features/features.js
@@ -1,5 +1,14 @@
 const NearPosition = require("./NearPosition");
 
-module.exports = [
+// features には pattern と run(message) を実装してください
+var features = [
     new NearPosition()
-];
+]
+
+features.forEach(feature => {
+    if (feature.pattern == undefined || feature.run == undefined) {
+        throw new Error("機能が発火用の正規表現とメソッドを持っていません。");
+    }
+})
+
+module.exports = features;
